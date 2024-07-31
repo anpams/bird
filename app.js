644aa85c81392159a463ade74f1dcd2d93bad210
anpams <anpa.eipi@gmail.com>
2024-07-31 16:03:10 +0200

    turned everything to functions Added lat and long as params Deleted date (not needed for recent observations)

--- app.js
+++ app.js
@@ -1,73 +1,60 @@
-
-const date = new Date();
-
-let day = date.getDay();
-let month = date.getMonth();
-let year = date.getFullYear();
-
-console.log(date.toLocaleDateString("en-US"));
-
-const recentObservations = async () => {
+async function recentObservations(lat, lng) {
     const myHeaders = new Headers();
     myHeaders.append("x-ebirdapitoken", "t67f2dq8gt9f");
-
     const requestOptions = {
-        method: "GET",
-        headers: myHeaders,
-        redirect: "follow"
+        method: "GET", headers: myHeaders, redirect: "follow"
     };
-
     try {
-        const response = await fetch("https://api.ebird.org/v2/data/obs/BE/recent/notable", requestOptions);
-        //change to lat and long for more accurate location. Or city
+        const url = "https://api.ebird.org/v2/data/obs/geo/recent/notable?lat=${lat}&lng=${lng}";
+//todo: fetch latlong
+        const response = await fetch(url, requestOptions);
         const data = await response.json();
         const sciName = data[0].sciName;
         console.log(sciName);
-        //fawait xenoCanto(sciName);
+        // await xenoCanto(sciName);
         await fetchTitle(sciName);
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("recent Observations error:", error);
     }
-};
+}
 
-const xenoCanto = async (sciName) => {
+
+async function xenoCanto(sciName) {
     const requestOptions = {
-        method: "GET",
-        headers: xenoCanto,
-        redirect: "follow"
+        method: "GET", headers: {
+            "Content-Type": "application/json"
+        }, redirect: "follow"
     };
 
-    const url = `https://xeno-canto.org/api/2/recordings?query=${encodeURIComponent(sciName)}`;
+    const url = "https://xeno-canto.org/api/2/recordings?query=${encodeURIComponent(sciName)}";
     try {
         const response = await fetch(url, requestOptions);
         const result = await response.text();
-        //just grab sound
+        // just grab sound
         console.log(result);
     } catch (error) {
-        console.error(error);
+        console.error("xenoCanto error", error);
     }
-};
+}
+
 
 function fetchTitle(sciName) { //gets title of wiki page
     var searchUrl = "https://en.wikipedia.org/w/api.php";
 
     var searchParams = {
-        action: "query",
-        list: "search",
-        srsearch: sciName,
-        format: "json"
+        action: "query", list: "search", srsearch: sciName, format: "json"
     };
 
     searchUrl = searchUrl + "?origin=*";
-    Object.keys(searchParams).forEach(function(key) {
+    Object.keys(searchParams).forEach(function (key) {
         searchUrl += "&" + key + "=" + searchParams[key];
     });
 
     fetch(searchUrl)
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
-        .then(function(response) {
+        .then(function (response) {
             var searchResults = response.query.search;
             if (searchResults.length > 0) {
                 var pageTitle = searchResults[0].title;
@@ -76,8 +63,8 @@ function fetchTitle(sciName) { //gets title of wiki page
                 console.log("No search results found for this term.");
             }
         })
-        .catch(function(error) {
-            console.log("Error fetching search results: ", error);
+        .catch(function (error) {
+            console.log("wikiTitle error: ", error);
         });
 }
 
@@ -85,23 +72,19 @@ function fetchImage(pageTitle) { //gets image from wikipage
     var url = "https://en.wikipedia.org/w/api.php";
 
     var params = {
-        action: "query",
-        prop: "pageimages",
-        titles: pageTitle,
-        format: "json",
-        pithumbsize: 500
+        action: "query", prop: "pageimages", titles: pageTitle, format: "json", pithumbsize: 500
     };
 
     url = url + "?origin=*";
-    Object.keys(params).forEach(function(key) {
+    Object.keys(params).forEach(function (key) {
         url += "&" + key + "=" + params[key];
     });
-
+    //todo: clean this up probably
     fetch(url)
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
-        .then(function(response) {
+        .then(function (response) {
             var pages = response.query.pages;
             for (var pageId in pages) {
                 var page = pages[pageId];
@@ -114,8 +97,8 @@ function fetchImage(pageTitle) { //gets image from wikipage
                 }
             }
         })
-        .catch(function(error) {
-            console.log("Error fetching the data: ", error);
+        .catch(function (error) {
+            console.log("Wiki image error: ", error);
         });
 }
 
